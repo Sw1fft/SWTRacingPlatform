843edeca34f368dcd6aa4aa00f4459f88a9b8ceb
swifft <testspam.vf@gmail.com>
2025-06-01 18:57:23 +0300

    Update PasswordService

--- IdentityService.Infrastructure/Services/PasswordService.cs
+++ IdentityService.Infrastructure/Services/PasswordService.cs
@@ -14,14 +14,14 @@ namespace IdentityService.Infrastructure.Services
 
         public Task<string> GeneratePassword(string password)
         {
-            string result = BCrypt.Net.BCrypt.EnhancedHashPassword(password, BCrypt.Net.HashType.SHA256);
+            string result = BCrypt.Net.BCrypt.EnhancedHashPassword(password);
 
             return Task.FromResult(result);
         }
 
         public Task<bool> VerifyPassword(string password, string hasedPassword)
         {
-            bool result = BCrypt.Net.BCrypt.EnhancedVerify(password, hasedPassword, BCrypt.Net.HashType.SHA256);
+            bool result = BCrypt.Net.BCrypt.EnhancedVerify(password, hasedPassword);
 
             return Task.FromResult(result);
         }
